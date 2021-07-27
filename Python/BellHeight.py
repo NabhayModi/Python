f1584464be14a3cb4a882b9122204d8192d313d5
NabhayModi <71711156+NabhayModi@users.noreply.github.com>
2021-07-27 20:14:31 +0530

    Update BellHeight.py

--- Python/BellHeight.py
+++ Python/BellHeight.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 import plotly.figure_factory as ff
 
-df =      pd.read_csv('HeightWeight.csv')
+df =      pd.read_csv('sudent id.csv')
 
 fig=ff.create_distplot([df["Height(Inches)"].tolist()],['Height'],show_hist=False)
 
